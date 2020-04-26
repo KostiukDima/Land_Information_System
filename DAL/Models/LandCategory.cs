03c030f19f74f980d2beab06995264adb918bdeb
Dima Kostiuk <DimaKostiuk@outlook.com>
2020-04-26 22:17:01 +0300

    Add LandLot Location Category Class

---
+++ DAL/Models/LandCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    class LandCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Navigation properties
+        public virtual ICollection<LandLot> LandLots { get; set; }
+    }
+}
---
+++ DAL/Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    class Location
+    {
+        public int Id { get; set; }
+        public string Region { get; set; }
+        public string District { get; set; }
+        public string Settlement { get; set; }
+        public string Street { get; set; }
+
+        // Navigation properties
+        public virtual LandLot LandLot { get; set; }
+    }
+}
